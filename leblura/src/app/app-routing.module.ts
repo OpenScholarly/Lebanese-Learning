d20226b35c5fc9ea9e9bb0b1d81353348392224a
MWE18 <mathieu.waharte@3ds.com>
2025-09-16 12:04:37 +0200

    base manual app

--- leblura/src/app/app-routing.module.ts
+++ leblura/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- leblura/src/app/app.module.ts
+++ leblura/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AppLayoutComponent } from './components/layout/app-layout/app-layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AppLayoutComponent,
+    DashboardComponent,
+    LandingComponent,
+    SignupComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,

9d19caf4aee74d676de68a289ead1774a83ee64e
MWE18 <mathieu.waharte@3ds.com>
2025-09-16 15:46:27 +0200

    programtical values

--- leblura/src/app/components/layout/header/header.component.ts
+++ leblura/src/app/components/layout/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { User } from 'src/app/interfaces/User';
 import { SidebarService } from 'src/app/services/sidebar.service';
+import { AppStore } from 'src/app/stores/app.store';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +10,19 @@ import { SidebarService } from 'src/app/services/sidebar.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  private destroy$ = new Subject<void>();
+  public user!: User;
 
-  constructor(public sidebarService: SidebarService) { }
+  constructor(public sidebarService: SidebarService, private appStore: AppStore) {
+    this.appStore.currentUser.pipe(takeUntil(this.destroy$)).subscribe((user) => {
+      if(user) {
+        this.user = user;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- leblura/src/app/components/profile/profile.component.ts
+++ leblura/src/app/components/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
+import { AppStore } from 'src/app/stores/app.store';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  private destroy$ = new Subject<void>();
+  public user!: User;
 
+  constructor(private appStore: AppStore) {
+    this.appStore.currentUser.pipe(takeUntil(this.destroy$)).subscribe((user) => {
+      if(user) {
+        this.user = user;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }

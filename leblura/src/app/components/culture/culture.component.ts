d446d22a7bdd09b502dec7e2936c849f9044a0a2
MWE18 <mathieu.waharte@3ds.com>
2025-09-16 18:05:10 +0200

    programatic culture page

--- leblura/src/app/components/culture/culture.component.ts
+++ leblura/src/app/components/culture/culture.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./culture.component.scss']
 })
 export class CultureComponent {
+  culturalTraditions = [
+    {
+      title: 'Traditional Dance',
+      category: 'traditions',
+      description: 'Experience the vibrant traditional dances that tell stories of our heritage.',
+      imageUrl: 'assets/images/traditional-dance.jpg'
+    },
+    {
+      title: 'Famous Figures',
+      category: 'famous-figures',
+      description: 'Learn about the influential figures who have shaped our culture and history.',
+      imageUrl: 'assets/images/famous-figures.jpg'
+    },
+    {
+      title: "Cuisine Culture",
+      category: 'cuisine',
+      description: "Explore the rich culinary traditions and dishes that define Lebanese cuisine.",
+      imageUrl: "assets/images/cuisine-culture.jpg"
+    },
+    {
+      title: "Music and Dance",
+      category: 'music',
+      description: "Discover the vibrant music and dance forms that are integral to Lebanese culture.",
+      imageUrl: "assets/images/music-dance.jpg"
+    },
+  ];
+  activeCategory = 'traditions';
+  activeTradition = this.culturalTraditions[0];
 
+  constructor() { }
+
+
+  setActiveCategory(category: string) {
+    this.activeCategory = category;
+    this.activeTradition = this.culturalTraditions.find(tradition => tradition.category === category) || this.culturalTraditions[0];
+  }
 }
